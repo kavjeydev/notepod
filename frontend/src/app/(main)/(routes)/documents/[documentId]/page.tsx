e5d4d7eb4ead9300d74f4f576ff1f15b83a1b938
kavjeydev <kavin_jey@Kavins-MacBook-Pro-2.local>
2024-10-25 20:25:43 -0600

    basic tiptap editor

--- frontend/src/app/(main)/(routes)/documents/[documentId]/page.tsx
+++ frontend/src/app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -4,6 +4,7 @@ import { useQuery } from "convex/react";
 import { Id } from "../../../../../../convex/_generated/dataModel";
 import { api } from "../../../../../../convex/_generated/api";
 import Toolbar from "@/components/toolbar";
+import Editor from "@/components/editor";
 
 interface DocumentIdPageProps {
   params: {
@@ -25,10 +26,11 @@ export default function DocumentIdPage({ params }: DocumentIdPageProps) {
   }
 
   return (
-    <div className="pb-40">
-      <div className="h-[35vh]"></div>
+    <div className="pb-0">
+      <div className="h-[10vh]"></div>
       <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
         <Toolbar initialData={document} />
+        <Editor />
       </div>
     </div>
   );
