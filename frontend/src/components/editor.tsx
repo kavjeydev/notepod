61f005c8ce8d4ada6fe10e829b3fbae3ecc0f7e7
kavjeydev <kavin_jey@Kavins-MacBook-Pro-2.local>
2024-10-26 21:51:04 -0600

    small changes

--- frontend/src/components/editor.tsx
+++ frontend/src/components/editor.tsx
@@ -301,41 +301,6 @@ const MenuBar = () => {
           </BubbleMenu>
         )}
 
-        {editor && (
-          <FloatingMenu
-            className="floating-menu"
-            tippyOptions={{ duration: 100 }}
-            editor={editor}
-          >
-            <button
-              onClick={() =>
-                editor.chain().focus().toggleHeading({ level: 1 }).run()
-              }
-              className={
-                editor.isActive("heading", { level: 1 }) ? "is-active" : ""
-              }
-            >
-              H1
-            </button>
-            <button
-              onClick={() =>
-                editor.chain().focus().toggleHeading({ level: 2 }).run()
-              }
-              className={
-                editor.isActive("heading", { level: 2 }) ? "is-active" : ""
-              }
-            >
-              H2
-            </button>
-            <button
-              onClick={() => editor.chain().focus().toggleBulletList().run()}
-              className={editor.isActive("bulletList") ? "is-active" : ""}
-            >
-              Bullet list
-            </button>
-          </FloatingMenu>
-        )}
-
         <EditorContent editor={editor} />
       </>
     </div>
