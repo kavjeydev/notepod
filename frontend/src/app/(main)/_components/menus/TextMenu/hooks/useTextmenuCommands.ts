18f421085a19f7798ff2e86602b25da29347865c
kavjeydev <kavin_jey@Kavins-MacBook-Pro-2.local>
2024-11-05 19:30:26 -0700

    prod changes

--- frontend/src/app/(main)/_components/menus/TextMenu/hooks/useTextmenuCommands.ts
+++ frontend/src/app/(main)/_components/menus/TextMenu/hooks/useTextmenuCommands.ts
@@ -71,87 +71,87 @@ export const useTextmenuCommands = (editor: Editor) => {
     [editor],
   );
 
-  const onSimplify = useCallback(
-    () =>
-      editor
-        .chain()
-        .focus()
-        .aiSimplify({ stream: true, format: "rich-text" })
-        .run(),
-    [editor],
-  );
-  const onEmojify = useCallback(
-    () =>
-      editor
-        .chain()
-        .focus()
-        .aiEmojify({ stream: true, format: "rich-text" })
-        .run(),
-    [editor],
-  );
-  const onCompleteSentence = useCallback(
-    () =>
-      editor
-        .chain()
-        .focus()
-        .aiComplete({ stream: true, format: "rich-text" })
-        .run(),
-    [editor],
-  );
-  const onFixSpelling = useCallback(
-    () =>
-      editor
-        .chain()
-        .focus()
-        .aiFixSpellingAndGrammar({ stream: true, format: "rich-text" })
-        .run(),
-    [editor],
-  );
-  const onMakeLonger = useCallback(
-    () =>
-      editor
-        .chain()
-        .focus()
-        .aiExtend({ stream: true, format: "rich-text" })
-        .run(),
-    [editor],
-  );
-  const onMakeShorter = useCallback(
-    () =>
-      editor
-        .chain()
-        .focus()
-        .aiShorten({ stream: true, format: "rich-text" })
-        .run(),
-    [editor],
-  );
-  const onTldr = useCallback(
-    () =>
-      editor
-        .chain()
-        .focus()
-        .aiTldr({ stream: true, format: "rich-text" })
-        .run(),
-    [editor],
-  );
-  const onTone = useCallback(
-    (tone: string) =>
-      editor
-        .chain()
-        .focus()
-        .aiAdjustTone(tone, { stream: true, format: "rich-text" })
-        .run(),
-    [editor],
-  );
-  const onTranslate = useCallback(
-    (language: Language) =>
-      editor
-        .chain()
-        .focus()
-        .aiTranslate(language, { stream: true, format: "rich-text" })
-        .run(),
-    [editor],
-  );
+  // const onSimplify = useCallback(
+  //   () =>
+  //     editor
+  //       .chain()
+  //       .focus()
+  //       .aiSimplify({ stream: true, format: "rich-text" })
+  //       .run(),
+  //   [editor],
+  // );
+  // const onEmojify = useCallback(
+  //   () =>
+  //     editor
+  //       .chain()
+  //       .focus()
+  //       .aiEmojify({ stream: true, format: "rich-text" })
+  //       .run(),
+  //   [editor],
+  // );
+  // const onCompleteSentence = useCallback(
+  //   () =>
+  //     editor
+  //       .chain()
+  //       .focus()
+  //       .aiComplete({ stream: true, format: "rich-text" })
+  //       .run(),
+  //   [editor],
+  // );
+  // const onFixSpelling = useCallback(
+  //   () =>
+  //     editor
+  //       .chain()
+  //       .focus()
+  //       .aiFixSpellingAndGrammar({ stream: true, format: "rich-text" })
+  //       .run(),
+  //   [editor],
+  // );
+  // const onMakeLonger = useCallback(
+  //   () =>
+  //     editor
+  //       .chain()
+  //       .focus()
+  //       .aiExtend({ stream: true, format: "rich-text" })
+  //       .run(),
+  //   [editor],
+  // );
+  // const onMakeShorter = useCallback(
+  //   () =>
+  //     editor
+  //       .chain()
+  //       .focus()
+  //       .aiShorten({ stream: true, format: "rich-text" })
+  //       .run(),
+  //   [editor],
+  // );
+  // const onTldr = useCallback(
+  //   () =>
+  //     editor
+  //       .chain()
+  //       .focus()
+  //       .aiTldr({ stream: true, format: "rich-text" })
+  //       .run(),
+  //   [editor],
+  // );
+  // const onTone = useCallback(
+  //   (tone: string) =>
+  //     editor
+  //       .chain()
+  //       .focus()
+  //       .aiAdjustTone(tone, { stream: true, format: "rich-text" })
+  //       .run(),
+  //   [editor],
+  // );
+  // const onTranslate = useCallback(
+  //   (language: Language) =>
+  //     editor
+  //       .chain()
+  //       .focus()
+  //       .aiTranslate(language, { stream: true, format: "rich-text" })
+  //       .run(),
+  //   [editor],
+  // );
   const onLink = useCallback(
     (url: string, inNewTab?: boolean) =>
       editor
@@ -201,15 +201,15 @@ export const useTextmenuCommands = (editor: Editor) => {
     onClearHighlight,
     onSetFont,
     onSetFontSize,
-    onSimplify,
-    onEmojify,
-    onCompleteSentence,
-    onFixSpelling,
-    onMakeLonger,
-    onMakeShorter,
-    onTldr,
-    onTone,
-    onTranslate,
+    // onSimplify,
+    // onEmojify,
+    // onCompleteSentence,
+    // onFixSpelling,
+    // onMakeLonger,
+    // onMakeShorter,
+    // onTldr,
+    // onTone,
+    // onTranslate,
     onLink,
   };
 };
