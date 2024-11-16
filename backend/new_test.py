fb3d09a05ba1ead3706fba0b897f638371fa1353
kavjeydev <kavin_jey@Kavins-MacBook-Pro-2.local>
2024-11-16 12:03:31 -0500

    constants error

--- backend/new_test.py
+++ backend/new_test.py
@@ -7,6 +7,7 @@ import shutil
 import sqlite3
 import time
 
+import constants
 import faiss
 import git
 import numpy as np
@@ -17,8 +18,6 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from tenacity import retry, stop_after_attempt, wait_random_exponential
 from tree_sitter import Language, Parser
-
-import backend.api.constants as constants
 
 # from sklearn.preprocessing import normalize
 
