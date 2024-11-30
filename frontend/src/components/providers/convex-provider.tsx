bc49ff0f83d942c49d6f3c70131240668bcf629e
kavjeydev <kavin_jey@Kavins-MacBook-Pro-2.local>
2024-11-29 19:21:49 -0700

    clerk prod

--- frontend/src/components/providers/convex-provider.tsx
+++ frontend/src/components/providers/convex-provider.tsx
@@ -4,12 +4,19 @@ import { ReactNode } from "react";
 import { ConvexReactClient } from "convex/react";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { ClerkProvider, useAuth } from "@clerk/clerk-react";
+import { useRouter } from "next/navigation";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
 export const ConvexClientProvider = ({ children }: { children: ReactNode }) => {
+  const router = useRouter();
   return (
     <ClerkProvider
       publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}
+      signInForceRedirectUrl="/documents"
+      signUpFallbackRedirectUrl="/documents"
+      // Ensure you specify the navigate callback if neede
+      // navigate={(to) => window.history.pushState(null, "", to)}
     >
       <ConvexProviderWithClerk useAuth={useAuth} client={convex}>
         {children}
