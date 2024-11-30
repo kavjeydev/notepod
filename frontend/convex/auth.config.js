df4693c432d438b4f15929de2912ca06b6248256
kavjeydev <kavin_jey@Kavins-MacBook-Pro-2.local>
2024-11-30 00:08:58 -0700

    convex test

--- frontend/convex/auth.config.js
+++ frontend/convex/auth.config.js
@@ -2,7 +2,8 @@ export default {
   providers: [
     {
       // domain: "https://capital-moth-69.clerk.accounts.dev",
-      domain: "https://clerk.notepod.co",
+      // domain: "https://clerk.notepod.co",
+      domain: "https://descriptive-mallard-599.convex.cloud",
       applicationID: "convex",
     },
   ],
