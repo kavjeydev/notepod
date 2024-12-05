b85020ae2a6480fb64a05c7742a5094af0fc9ab9
kavjeydev <kavin_jey@Kavins-MacBook-Pro-2.local>
2024-12-04 20:27:36 -0700

    overflow fix

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body
         suppressHydrationWarning
-        className="dark:bg-[#121212] h-full bg-[#FFFFFF]"
+        className="dark:bg-[#121212] h-full bg-[#FFFFFF] overflow-x-hidden"
       >
         <ConvexClientProvider>
           <ThemeProvider
