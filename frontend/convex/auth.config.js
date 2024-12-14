c97d28f317ea6c7b7b16b55b7187b90066bee4b1
kavjeydev <kavin_jey@Kavins-MacBook-Pro-2.local>
2024-12-13 20:37:57 -0700

    re-enable clerk

--- frontend/convex/auth.config.js
+++ frontend/convex/auth.config.js
@@ -1,8 +1,8 @@
 export default {
   providers: [
     {
-      domain: "https://capital-moth-69.clerk.accounts.dev",
-      // domain: "https://clerk.notepod.co",
+      // domain: "https://capital-moth-69.clerk.accounts.dev",
+      domain: "https://clerk.notepod.co",
       // domain: "https://descriptive-mallard-599.convex.cloud",
       applicationID: "convex",
     },
